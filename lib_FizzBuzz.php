525d499947f0d6d657efdd7ca81e3beb3b9c6be1
Fedmich <fedmich@gmail.com>
2014-02-17 16:38:18 +0800

    from what I remember the "Bazz" is modulo 15... though the instruction says differently though.. hmm..

--- lib_FizzBuzz.php
+++ lib_FizzBuzz.php
@@ -2,14 +2,16 @@
 
 /*
  * @author: federico
- * @version: 0.1
+ * @version: 0.2 (Solution 2)
  */
 
 function FizzBuzz($from, $to) {
 	$ret = array();
 	foreach (range($from, $to) as $i) {
-
-		if ($i % 3 == 0) {
+		if ($i % 15 == 0) {
+			$fz = 'Bazz';
+		}
+		else if ($i % 3 == 0) {
 			$fz = 'Fizz';
 		} else if ($i % 5 == 0) {
 			$fz = 'Buzz';
