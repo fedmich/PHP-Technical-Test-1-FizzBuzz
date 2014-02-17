801f6e23b0d2409b7821749cd9dea7b3faf35ff5
Fedmich <fedmich@gmail.com>
2014-02-17 15:48:38 +0800

    Base UI for solution

---
+++ solution1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#" >
+	<head>
+		<title>Solution 1</title>
+	</head>
+	<body>
+		<h1>FizzBuzz Solution 1</h1>
+		<form name="" action="">
+			Number 1: <input type="text" id="number_1" name="number_1" value="" />
+			<br />
+			Number 2: <input type="text" id="number_2" name="number_2" value="" />
+			<br />
+			<input type="submit" value="Go" />
+		</form>
+
+	</body>
+</html>
